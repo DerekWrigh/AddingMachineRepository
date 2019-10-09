3ecfbb4b8d8b30563f237ab2be2fda1d23682980
Derek <Derek@DESKTOP-GTGFFMP>
2019-10-09 10:53:10 -0700

    Initial Version Commit

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,54 @@
+/**
+ * Derek Wright
+ * Class ID: 70641
+ * Assignment 2
+ */
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total;
+	
+	/**
+	 * the adding machine
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * gets the current total.
+	 * @return total.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * adds a value to the current total.
+	 * @param value being added.
+	 */
+	public void add (int value) {
+	}
+	
+	/**
+	 * subtracts a value to the current total.
+	 * @param value being subtracted.
+	 */
+	public void subtract (int value) {
+	}
+	
+	/**
+	 * makes a string of all the commands that were input.
+	 * @return string of commands inputed.
+	 */
+	public String toString () {
+		return "";
+	}
+	
+	/**
+	 * clears out the command list and resets the total to 0.
+	 */
+	public void clear() {
+	}
+}
